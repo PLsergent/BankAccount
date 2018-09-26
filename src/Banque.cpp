08327cfb480f67c50ea5ebfd6b2c138891f8fed7
papalouis <papa.louis59@gmail.com>
2018-09-26 16:57:08 +0200

    function redimensionne done

--- src/Banque.cpp
+++ src/Banque.cpp
@@ -30,10 +30,35 @@ void Banque::ajouteCompte(string cpt, float soldeCpt, float decouvMax)
 
 void Banque::supprimeCompte(string nomCompte){
     for (int i = 0; i < nbc; i++){
-      if (nomCompte == comptes[i]->getNomCompte()){
+      if (nomCompte == comptes[i].getNomCompte()){
         for (int j = i; j < nbc; j++){
-          compte[j] = compte[j+1];
+          comptes[j] = comptes[j+1];
         }
       }
     }
+}
+
+void Banque::supprimeCompte(int numCompte){
+    for (int i = 0; i < nbc; i++){
+      if (numCompte == comptes[i].getNumeroCompte()){
+        for (int j = i; j < nbc; j++){
+          comptes[j] = comptes[j+1];
+        }
+      }
+    }
+}
+
+void Banque::redimensionneTableauComptes(int nouvelleTaille){
+    Compte* newcomptes = new Compte[nouvelleTaille];
+    if (nouvelleTaille < nbc){
+      for (int i = 0; i < nouvelleTaille; i++){
+        newcomptes[i] = comptes[i];
+      }
+    }else{
+      for (int i = 0; i < nbc; i++){
+        newcomptes[i] = comptes[i];
+      }
+    }
+    delete [] comptes;
+    comptes = newcomptes;
 }
